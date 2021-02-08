0413b63e965e0df59aed06ec46daafd7b6af37b0
michaljarnot <m.jarnot@yahoo.com>
2021-02-08 21:27:10 +0100

    chore: remove unnecessary router

--- apps/opava/src/app/app.module.ts
+++ apps/opava/src/app/app.module.ts
@@ -2,7 +2,6 @@ import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 
 import { AppComponent } from './app.component';
-import { RouterModule } from '@angular/router';
 import { MenuModule } from '@zoos/menu';
 import { FishModule } from '@zoos/fish';
 import { BirdsModule } from '@zoos/birds';
@@ -10,14 +9,7 @@ import { MammalsModule } from '@zoos/mammals';
 
 @NgModule({
   declarations: [AppComponent],
-  imports: [
-    BrowserModule,
-    RouterModule.forRoot([], { initialNavigation: 'enabled' }),
-    MenuModule,
-    FishModule,
-    BirdsModule,
-    MammalsModule,
-  ],
+  imports: [BrowserModule, MenuModule, FishModule, BirdsModule, MammalsModule],
   providers: [],
   bootstrap: [AppComponent],
 })
